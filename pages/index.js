ade100f33899c7145bc0f4a04efc5b3b459effea
Sidak Harnoor Singh <z5525050@ad.unsw.edu.au>
2025-06-28 02:31:41 +1000

    added projects

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
           <TextHoverEffect text="Sidak" />
         </div>
         <p className="mt-40 text-gray-300 text-2xl font-medium relative z-10">
-          Aspiring Software Engineer | Building Cool Things 🚀
+          Aspiring Software Engineer | Building Cool Things
         </p>
       </section>
 
@@ -19,20 +19,20 @@ export default function Home() {
       <section className="flex flex-col items-start mt-30 px-20">
         <h2 className="text-8xl font-bold mb-4">About Me</h2>
         <p className="max-w-9xl text-left text-gray-400 mb-6 text-xl">
-          I am Sidak, a second-year Computer Engineering student passionate
+          I am Sidak, a second-year Computer Science student passionate
           about web development, software, and building cool projects that make
-          a difference.
+          a difference. I am in my learning and exploring phase right now.
         </p>
         <div className="flex gap-6">
           <a
-            href="https://github.com/YOUR_GITHUB"
+            href="https://github.com/sidak005"
             target="_blank"
             className="bg-white text-black px-6 py-2 rounded-full hover:bg-gray-300 transition"
           >
             GitHub
           </a>
           <a
-            href="https://linkedin.com/in/YOUR_LINKEDIN"
+            href="https://www.linkedin.com/in/sidak-harnoor-singh-853aa2294/"
             target="_blank"
             className="bg-white text-black px-6 py-2 rounded-full hover:bg-gray-300 transition"
           >
@@ -60,11 +60,53 @@ export default function Home() {
               encryption and a user-friendly GUI.
             </p>
             <a
-              href="https://github.com/YOUR_GITHUB/password-manager"
+              href="https://github.com/sidak005/password-manager"
               target="_blank"
               className="text-blue-400 underline"
             >
-              View Project
+              View here
+            </a>
+          </div>
+          {/* movie card */}
+          <div className="p-6 bg-gray-800 rounded-xl hover:scale-105 transition-transform">
+              <h3 className="text-2xl font-semibold mb-2">Movie List</h3>
+              <p className="text-gray-400 mb-4">
+              A very basic project to get an understanding of react by using an example of a movie list.
+              </p>  
+            <a
+              href="https://github.com/sidak005/react-movieList"
+              target="_blank"
+              className="text-blue-400 underline"
+            >
+              View here
+            </a>
+          </div>
+          {/* weather app */}
+          <div className="p-6 bg-gray-800 rounded-xl hover:scale-105 transition-transform">
+              <h3 className="text-2xl font-semibold mb-2">Weather App</h3>
+              <p className="text-gray-400 mb-4">
+              A weather app to get to know about the current weather as well as forecast for next days.
+              </p>  
+            <a
+              href="https://github.com/sidak005/react-movieList"
+              target="_blank"
+              className="text-blue-400 underline"
+            >
+              View here
+            </a>
+          </div>
+          {/* coming soon */}
+          <div className="p-6 bg-gray-800 rounded-xl hover:scale-105 transition-transform">
+              <h3 className="text-2xl font-semibold mb-2">Coming soon</h3>
+              <p className="text-gray-400 mb-4">
+              More projects to be added soon!!
+              </p>  
+            <a
+              href="https://github.com/sidak005/react-movieList"
+              target="_blank"
+              className="text-blue-400 underline"
+            >
+              🔜🔜
             </a>
           </div>
         </div>
